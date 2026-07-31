21c873e0fe4fc83fbaeb1864bc04c2567f6507a9
Leetcode-Practice <shubhamagrawal928013@gmail.com>
2026-07-31 20:22:24 +0530

    Time: 1 ms (86.83%) | Memory: 44.3 MB (20.60%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,32 @@
+// class Solution {
+//     public List<List<Integer>> subsets(int[] arr) {
+//           int n =arr.length;
+//         int m = (1<<n);
+//         List<List<Integer>> ans = new ArrayList<>();
+//         for(int i=0;i<m;i++){
+//             List<Integer> a = new ArrayList<>();
+//             for(int j=0;j<n;j++){
+//                 if((i>>j)%2==1) a.add(arr[j]);
+//             }
+//             ans.add(a);
+//         }
+//         return ans;
+//     }
+// }
+
+
+class Solution{
+    public List<List<Integer>> subsets(int[] arr){
+        int n = arr.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        int subsets =1<<n;
+        for(int i=0;i<subsets;i++){
+            List<Integer> a = new ArrayList<>();
+            for(int j=0;j<n;j++){
+            if((i>>j)%2==1) a.add(arr[j]);
+            }
+            ans.add(a);
+        }
+        return ans;
+    }
+}
