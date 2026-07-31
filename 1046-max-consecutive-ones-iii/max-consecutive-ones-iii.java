eaf8ba95af81dbd5d7ecc55c856f85fc4ba9ce59
Leetcode-Practice <shubhamagrawal928013@gmail.com>
2026-07-31 23:57:55 +0530

    Time: 4 ms (44.27%) | Memory: 51.7 MB (98.26%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,38 +1,48 @@
-class Solution {
-    public int longestOnes(int[] nums, int k) {
-        // int left =0
-        // int right =0;
-        // int zeroCount =0;
-        // int maxOnes =0;
+// Brute Force Approach
 
-        // while(right<nums.length){
-        //     if(nums[left]==1 && nums[right]==1){
-        //         right++;
-        //         maxOnes = Math.max(maxOnes , right-left)
-        //     }
-        //     else if(nums[right]==0){
-        //     left=right;
-        //     if(right-left<k){
-        //         right++;
-        //         zeoOnes = M
-        //     }
-        //     }
-        // }
+// class Solution {
+//     public int longestOnes(int[] nums, int k) {
+//         int maxLen =0;
+//         for(int i=0;i<nums.length;i++){
+//             int zeros=0;
+//             for(int j=i;j<nums.length;j++){
+//                 if(nums[j]==0){
+//                     zeros++;
+//                 }
+//                 if(zeros>k){
+//                     break;
+//                 }
+//                 maxLen = Math.max(maxLen, j-i+1);
+//             }
+//         }
+//         return maxLen;
 
+//     }
+// }
+
+
+// Optimal Solution
+
+class Solution{
+    public int longestOnes(int nums[] , int k){
+        int left =0;
+        int zeros =0;
         int maxLen =0;
-        for(int i=0;i<nums.length;i++){
-            int zeros=0;
-            for(int j=i;j<nums.length;j++){
-                if(nums[j]==0){
-                    zeros++;
-                }
-                if(zeros>k){
-                    break;
-                }
-                maxLen = Math.max(maxLen, j-i+1);
+        for(int right =0; right < nums.length; right++){
+            if(nums[right]==0){
+                zeros++;
             }
+            if(zeros>k){
+             if(nums[left]==0){
+                zeros--;
+             }
+              left++;
+             
+            }
+                         maxLen = Math.max(maxLen, right-left+1);
+
         }
-        return maxLen;
+                    return maxLen;
 
     }
 }
