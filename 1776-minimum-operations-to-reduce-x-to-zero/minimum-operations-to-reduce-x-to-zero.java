a29c411428ee6d3a30c89b3867934c8d2ecae7ec
Leetcode-Practice <shubhamagrawal928013@gmail.com>
2026-09-23 19:31:05 +0530

    Time: 4 ms (98.07%) | Memory: 102 MB (59.92%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.java
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.java
@@ -1,31 +1,24 @@
 class Solution {
     public int minOperations(int[] nums, int x) {
         int n = nums.length;
-
         int total = 0;
         for (int num : nums) {
             total += num;
         }
-
         int target = total - x;
-
         // We need to remove everything
         if (target == 0) {
             return n;
         }
-
         // Impossible
         if (target < 0) {
             return -1;
-        }
-
+        }        
         int left = 0;
         int sum = 0;
         int maxLen = -1;
-
         for (int right = 0; right < n; right++) {
             sum += nums[right];
-
             // Shrink window if sum becomes too large
             while (left <= right && sum > target) {
                 sum -= nums[left];
@@ -36,11 +29,9 @@ class Solution {
                 maxLen = Math.max(maxLen, right - left + 1);
             }
         }
-
         if (maxLen == -1) {
             return -1;
         }
-
         return n - maxLen;
     }
 }
